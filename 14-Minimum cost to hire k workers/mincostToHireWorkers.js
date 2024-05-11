bac9d23c4393eda5ff724ca3e7ea196a3402edac
RisvanBCA2022 <mohammedrisvan16@gmail.com>
2024-05-11 12:45:20 +0530

    minimum cost to hire workers

---
+++ 14-Minimum cost to hire k workers/mincostToHireWorkers.js
@@ -0,0 +1,13 @@
+const quality = [10,20,5]
+const wage = [70,50,30]
+const k = 2
+const quality1 = [3,1,10,10,1]
+const wage1 = [4,8,2,2,7]
+const k1 = 3
+
+var mincostToHireWorkers = function(quality, wage, k) {
+    
+};
+
+console.log(mincostToHireWorkers(quality,wage,k));
+console.log(mincostToHireWorkers(quality1,wage1,k1));
